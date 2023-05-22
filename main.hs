a95a16cd05d1aa3f0f1e97695748be20dac739a8
Pedro <pedroalmeidavrb@gmail.com>
2023-05-21 22:03:40 -0300

    Add modulos e gitignore

--- main.hs
+++ main.hs
@@ -1,33 +1,62 @@
+module Minefield
+    ( Minefield(..)
+    , readMinefield
+    ) where
+
+data Minefield = Minefield
+    { numLines :: Int
+    , numColumns :: Int
+    , numBombs :: Int
+    }
+
 validateNumBombs :: Int -> Int -> Int -> Bool
 validateNumBombs numLines numColumns numBombs =
   numBombs <= (numLines * numColumns) `div` 2
 
-main:: IO()
-main = do
+readMinefield :: IO Minefield
+readMinefield = do
     putStrLn "--------------------------------------------------"
     putStrLn "-----------------CAMPO MINADO---------------------"
     putStrLn "--------------------------------------------------"
 
-    putStrLn "Digite o número de linhas: "
+    putStrLn "Digite o número de linhas: \n"
     numLinesStr <- getLine
     let numLines = read numLinesStr :: Int
 
-    putStrLn "Digite o número de colunas: "
+    putStrLn "Digite o número de colunas: \n"
     numColumnsStr <- getLine
     let numColumns = read numColumnsStr :: Int
 
     let getNumBombs = do
-          putStrLn "Digite o número de bombas: "
+          putStrLn "Digite o número de bombas: \n"
           numBombsStr <- getLine
           let numBombs = read numBombsStr :: Int
           let isValidNumBombs = validateNumBombs numLines numColumns numBombs
           if not isValidNumBombs
               then do
-                  putStrLn "O número de bombas deve ser metade do tamanho do tabuleiro!"
+                  putStrLn "O número de bombas deve ser metade do tamanho do tabuleiro! \n"
                   getNumBombs
               else return numBombs
 
     numBombs <- getNumBombs
 
-    putStrLn $ "Número de bombas válido: " ++ show numBombs
+    return Minefield
+        { numLines = numLines
+        , numColumns = numColumns
+        , numBombs = numBombs
+        }
 
+main :: IO ()
+main = do
+    minefield <- readMinefield
+
+    putStrLn $ "Número de bombas válido! \n" ++ show (numBombs minefield)
+
+getNumLines :: Minefield -> Int
+getNumLines = numLines
+
+getNumColumns :: Minefield -> Int
+getNumColumns = numColumns
+
+getNumBombs :: Minefield -> Int
+getNumBombs = numBombs
