a6861078e9f156fa0246987c73d5f006ff0ec898
Pedro <pedroalmeidavrb@gmail.com>
2023-06-10 14:33:16 -0300

    fazendo as jogadas

--- app/Main.hs
+++ app/Main.hs
@@ -47,15 +47,91 @@ printBoard (MinesweeperBoard _ _ cells) =
   mapM_ (putStrLn . concatMap cellToChar) cells
   where
     cellToChar (Cell True _ _ _) = "X "
-    cellToChar (Cell False isOpen _ nearbyMines) = if isOpen then show nearbyMines ++ " " else "* "
+    cellToChar (Cell False isOpen False nearbyMines) = if isOpen then show nearbyMines ++ " " else "* "
+    cellToChar (Cell False _ True _) = "B "
 
 openCell :: (Int, Int) -> MinesweeperBoard -> MinesweeperBoard
 openCell (row, col) (MinesweeperBoard size count cells) =
   if isOpen cell || isFlagged cell
     then MinesweeperBoard size count cells
-    else MinesweeperBoard size count (updateCell row col (\cell -> cell { isOpen = True }) cells)
+    else MinesweeperBoard size count (updateCell row col (\cell -> cell { isOpen = True }) cells')
   where
     cell = cells !! row !! col
+    cells' = updateAdjacentCells (row, col) cells
+
+flagCell :: (Int, Int) -> MinesweeperBoard -> MinesweeperBoard
+flagCell (row, col) (MinesweeperBoard size count cells) =
+  if isOpen cell
+    then MinesweeperBoard size count cells
+    else MinesweeperBoard size (count + if isFlagged cell then -1 else 1) (updateCell row col toggleFlag cells)
+  where
+    cell = cells !! row !! col
+    toggleFlag c = c { isFlagged = not (isFlagged c) }
+
+updateAdjacentCells :: (Int, Int) -> [[Cell]] -> [[Cell]]
+updateAdjacentCells (row, col) cells = updateCell row col updateAdjacent cells
+  where
+    updateAdjacent cell =
+      if isMine cell
+        then cell
+        else cell { nearbyMines = countAdjacentMines (row, col) cells }
+
+countAdjacentMines :: (Int, Int) -> [[Cell]] -> Int
+countAdjacentMines (row, col) cells = length $ filter isMine adjacentCells
+  where
+    adjacentCells = [ cells !! i !! j | i <- [row-1..row+1], j <- [col-1..col+1], isValidCell (i, j) ]
+    isValidCell (i, j) = i >= 0 && i < numRows && j >= 0 && j < numCols
+    numRows = length cells
+    numCols = length (head cells)
+
+validatePosition :: (Int, Int) -> MinesweeperBoard -> Bool
+validatePosition (row, col) (MinesweeperBoard (numLines, numColumns) _ _) =
+  row >= 0 && row < numLines && col >= 0 && col < numColumns
+
+playGame :: MinesweeperBoard -> IO ()
+playGame board = do
+  putStrLn "Digite o comando (o para abrir, m para marcar, d para desmarcar):"
+  command <- getLine
+  case command of
+    "o" -> do
+      putStrLn "Digite a posição para abrir (linha coluna):"
+      positionStr <- getLine
+      let [row, col] = map read (words positionStr) :: [Int]
+      if validatePosition (row, col) board
+        then do
+          let newBoard = openCell (row, col) board
+          printBoard newBoard
+          playGame newBoard
+        else do
+          putStrLn "Posição inválida!"
+          playGame board
+    "m" -> do
+      putStrLn "Digite a posição para marcar (linha coluna):"
+      positionStr <- getLine
+      let [row, col] = map read (words positionStr) :: [Int]
+      if validatePosition (row, col) board
+        then do
+          let newBoard = flagCell (row, col) board
+          printBoard newBoard
+          playGame newBoard
+        else do
+          putStrLn "Posição inválida!"
+          playGame board
+    "d" -> do
+      putStrLn "Digite a posição para desmarcar (linha coluna):"
+      positionStr <- getLine
+      let [row, col] = map read (words positionStr) :: [Int]
+      if validatePosition (row, col) board
+        then do
+          let newBoard = flagCell (row, col) board
+          printBoard newBoard
+          playGame newBoard
+        else do
+          putStrLn "Posição inválida!"
+          playGame board
+    _ -> do
+      putStrLn "Comando inválido!"
+      playGame board
 
 main :: IO ()
 main = do
@@ -88,9 +164,4 @@ main = do
 
   board <- initBoard (numLines, numColumns) numBombs
   printBoard board
-
-  putStrLn "Digite a posição para abrir (linha coluna):"
-  positionStr <- getLine
-  let [row, col] = map read (words positionStr) :: [Int]
-      newBoard = openCell (row, col) board
-  printBoard newBoard
+  playGame board
